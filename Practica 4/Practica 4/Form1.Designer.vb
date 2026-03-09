b36e47232d738fd526cbdcb9eeafef647daf0583
ethan-garcia <ethan.garcia@iest.edu.mx>
2026-03-09 08:41:31 -0600

    Add files via upload

---
+++ Practica 4/Practica 4/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Horas = New System.Windows.Forms.Label()
+        Me.Minutos = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Dia = New System.Windows.Forms.Label()
+        Me.Fecha = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Horas
+        '
+        Me.Horas.AutoSize = True
+        Me.Horas.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Horas.Location = New System.Drawing.Point(59, 91)
+        Me.Horas.Name = "Horas"
+        Me.Horas.Size = New System.Drawing.Size(78, 55)
+        Me.Horas.TabIndex = 0
+        Me.Horas.Text = "00"
+        '
+        'Minutos
+        '
+        Me.Minutos.AutoSize = True
+        Me.Minutos.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Minutos.Location = New System.Drawing.Point(186, 91)
+        Me.Minutos.Name = "Minutos"
+        Me.Minutos.Size = New System.Drawing.Size(78, 55)
+        Me.Minutos.TabIndex = 1
+        Me.Minutos.Text = "00"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(143, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 55)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = ":"
+        '
+        'Dia
+        '
+        Me.Dia.AutoSize = True
+        Me.Dia.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Dia.Location = New System.Drawing.Point(66, 172)
+        Me.Dia.Name = "Dia"
+        Me.Dia.Size = New System.Drawing.Size(169, 55)
+        Me.Dia.TabIndex = 3
+        Me.Dia.Text = "Lunes "
+        '
+        'Fecha
+        '
+        Me.Fecha.AutoSize = True
+        Me.Fecha.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Fecha.Location = New System.Drawing.Point(69, 244)
+        Me.Fecha.Name = "Fecha"
+        Me.Fecha.Size = New System.Drawing.Size(195, 37)
+        Me.Fecha.TabIndex = 4
+        Me.Fecha.Text = "09 de Marzo"
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Fecha)
+        Me.Controls.Add(Me.Dia)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Minutos)
+        Me.Controls.Add(Me.Horas)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Horas As Label
+    Friend WithEvents Minutos As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Dia As Label
+    Friend WithEvents Fecha As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Practica 4/Practica 4/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim Date_o = DateTime.Now.ToString("dd 'de' MMMM 'del' yyyy")
+        Dim Day_o = DateTime.Now.ToString("dddd")
+        Dim time_o = DateTime.Now.ToString("HH:mm")
+
+        Dim time_mod = time_o.Split(":")
+
+        Horas.text = time_mod(0)
+        Minutos.text = time_mod(1)
+        Dia.Text = Day_o
+        Fecha.text = Date_o
+    End Sub
+End Class
