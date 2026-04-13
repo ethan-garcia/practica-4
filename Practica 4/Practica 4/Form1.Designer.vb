a6de3776974020bece138deba8b746e5cc0aa517
ethan-garcia <ethan.garcia@iest.edu.mx>
2026-04-13 08:41:53 -0600

    Add files via upload

--- Practica 4/Practica 4/Form1.Designer.vb
+++ Practica 4/Practica 4/Form1.Designer.vb
@@ -23,19 +23,46 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Horas = New System.Windows.Forms.Label()
         Me.Minutos = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Dia = New System.Windows.Forms.Label()
         Me.Fecha = New System.Windows.Forms.Label()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Segundos = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Horas
         '
         Me.Horas.AutoSize = True
+        Me.Horas.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.Horas.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Horas.Location = New System.Drawing.Point(59, 91)
+        Me.Horas.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Horas.Location = New System.Drawing.Point(339, 111)
         Me.Horas.Name = "Horas"
         Me.Horas.Size = New System.Drawing.Size(78, 55)
         Me.Horas.TabIndex = 0
@@ -44,8 +71,10 @@ Partial Class Form1
         'Minutos
         '
         Me.Minutos.AutoSize = True
+        Me.Minutos.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.Minutos.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Minutos.Location = New System.Drawing.Point(186, 91)
+        Me.Minutos.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Minutos.Location = New System.Drawing.Point(466, 111)
         Me.Minutos.Name = "Minutos"
         Me.Minutos.Size = New System.Drawing.Size(78, 55)
         Me.Minutos.TabIndex = 1
@@ -54,8 +83,10 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(143, 91)
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label3.Location = New System.Drawing.Point(423, 111)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(37, 55)
         Me.Label3.TabIndex = 2
@@ -64,8 +95,10 @@ Partial Class Form1
         'Dia
         '
         Me.Dia.AutoSize = True
+        Me.Dia.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.Dia.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Dia.Location = New System.Drawing.Point(66, 172)
+        Me.Dia.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Dia.Location = New System.Drawing.Point(346, 192)
         Me.Dia.Name = "Dia"
         Me.Dia.Size = New System.Drawing.Size(169, 55)
         Me.Dia.TabIndex = 3
@@ -74,28 +107,203 @@ Partial Class Form1
         'Fecha
         '
         Me.Fecha.AutoSize = True
-        Me.Fecha.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Fecha.Location = New System.Drawing.Point(69, 244)
+        Me.Fecha.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Fecha.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Fecha.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Fecha.Location = New System.Drawing.Point(329, 247)
         Me.Fecha.Name = "Fecha"
-        Me.Fecha.Size = New System.Drawing.Size(195, 37)
+        Me.Fecha.Size = New System.Drawing.Size(186, 25)
         Me.Fecha.TabIndex = 4
-        Me.Fecha.Text = "09 de Marzo"
+        Me.Fecha.Text = "09 de Marzo 2026"
         '
         'Timer1
         '
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Location = New System.Drawing.Point(334, 93)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(233, 182)
+        Me.Panel1.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(13, 60)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Label4"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(16, 118)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(135, 118)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Cronometro"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Blanco ", "Rojo", "Azul", "Amarillo", "Verde"})
+        Me.ComboBox1.Location = New System.Drawing.Point(18, 76)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 52)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(49, 21)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Configuracion"
+        '
+        'Segundos
+        '
+        Me.Segundos.AutoSize = True
+        Me.Segundos.Location = New System.Drawing.Point(505, 80)
+        Me.Segundos.Name = "Segundos"
+        Me.Segundos.Size = New System.Drawing.Size(39, 13)
+        Me.Segundos.TabIndex = 3
+        Me.Segundos.Text = "Label2"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.ComboBox1)
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Location = New System.Drawing.Point(334, 96)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(228, 179)
+        Me.Panel2.TabIndex = 3
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.Label5)
+        Me.Panel3.Location = New System.Drawing.Point(334, 93)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(233, 182)
+        Me.Panel3.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(85, 16)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(71, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Temporizador"
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.Practica_4.My.Resources.Resources.temporixajdor
+        Me.PictureBox4.Location = New System.Drawing.Point(469, 290)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(52, 37)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 9
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.ErrorImage = Global.Practica_4.My.Resources.Resources.Sin_título3
+        Me.PictureBox3.Image = Global.Practica_4.My.Resources.Resources.Sin_título2
+        Me.PictureBox3.Location = New System.Drawing.Point(408, 290)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(52, 37)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox3.TabIndex = 8
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.ErrorImage = Global.Practica_4.My.Resources.Resources.services_parametres_et_icone_d_engrenage_noir
+        Me.PictureBox2.Image = Global.Practica_4.My.Resources.Resources.services_parametres_et_icone_d_engrenage_noir
+        Me.PictureBox2.Location = New System.Drawing.Point(350, 278)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(52, 58)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 6
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-76, -111)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(992, 611)
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Segundos)
+        Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.Fecha)
         Me.Controls.Add(Me.Dia)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Minutos)
         Me.Controls.Add(Me.Horas)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -107,4 +315,21 @@ Partial Class Form1
     Friend WithEvents Dia As Label
     Friend WithEvents Fecha As Label
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Segundos As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Label5 As Label
+    Friend WithEvents PictureBox4 As PictureBox
 End Class
--- Practica 4/Practica 4/Form1.vb
+++ Practica 4/Practica 4/Form1.vb
@@ -1,22 +1,125 @@
 ﻿Public Class Form1
+    Dim formato As Boolean = True
     Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
 
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Timer1.Start()
+        Timer2.Start()
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Dim Date_o = DateTime.Now.ToString("dd 'de' MMMM 'del' yyyy")
         Dim Day_o = DateTime.Now.ToString("dddd")
-        Dim time_o = DateTime.Now.ToString("HH:mm")
+        Dim Time_o As String = ""
 
-        Dim time_mod = time_o.Split(":")
 
-        Horas.text = time_mod(0)
-        Minutos.text = time_mod(1)
+        If formato = True Then
+            Time_o = DateTime.Now.ToString("HH:mm:ss")
+        ElseIf formato = False Then
+            Time_o = DateTime.Now.ToString("hh:mm:ss")
+        End If
+
+        Dim Time_mod = Time_o.Split(":")
+
+        Horas.Text = Time_mod(0)
+        Minutos.Text = Time_mod(1)
         Dia.Text = Day_o
-        Fecha.text = Date_o
+        Fecha.Text = Date_o
+        Segundos.text = Time_mod(2)
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If Label3.Text = ":" Then
+            Label3.Text = ""
+        ElseIf Label3.Text = "" Then
+            Label3.Text = ":"
+        End If
+    End Sub
+    Private Sub Fecha_Click(sender As Object, e As EventArgs) Handles Fecha.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        If Panel2.Visible = False Then
+            Panel2.Visible = True
+        ElseIf Panel2.Visible = True Then
+            Panel2.Visible = False
+        End If
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim color_texto As String = ComboBox1.SelectedItem.ToString()
+        If color_texto = "Blanco" Then
+            Horas.ForeColor = Color.White
+            Minutos.ForeColor = Color.White
+            Label3.ForeColor = Color.White
+            Fecha.ForeColor = Color.White
+            Dia.ForeColor = Color.White
+        ElseIf color_texto = "Rojo" Then
+            Horas.ForeColor = Color.Red
+            Minutos.ForeColor = Color.Red
+            Label3.ForeColor = Color.Red
+            Fecha.ForeColor = Color.Red
+            Dia.ForeColor = Color.Red
+        ElseIf color_texto = "Amarillo" Then
+            Horas.ForeColor = Color.Yellow
+            Minutos.ForeColor = Color.Yellow
+            Label3.ForeColor = Color.Yellow
+            Fecha.ForeColor = Color.Yellow
+            Dia.ForeColor = Color.Yellow
+        ElseIf color_texto = "Azul" Then
+            Horas.ForeColor = Color.Blue
+            Minutos.ForeColor = Color.Blue
+            Label3.ForeColor = Color.Blue
+            Fecha.ForeColor = Color.Blue
+            Dia.ForeColor = Color.Blue
+        ElseIf color_texto = "Verde" Then
+            Horas.ForeColor = Color.Green
+            Minutos.ForeColor = Color.Green
+            Label3.ForeColor = Color.Green
+            Fecha.ForeColor = Color.Green
+            Dia.ForeColor = Color.Green
+        End If
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If formato = True Then
+            Button1.Text = "12h"
+            formato = False
+        ElseIf formato = False Then
+            Button1.Text = "24h"
+            formato = True
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        If Panel1.Visible = True Then
+            Panel1.Visible = False
+        ElseIf Panel1.Visible = False Then
+            Panel1.Visible = True
+        End If
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+        If Panel3.Visible = True Then
+            Panel3.Visible = False
+        ElseIf Panel3.Visible = False Then
+            Panel3.Visible = True
+        End If
     End Sub
 End Class
